c09f14d4af8db529f120368e0325743594c22688
lynk <danghoanglinh161994@gmail.com>
2016-04-06 01:16:01 +0700

    like buttom

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,10 @@
 class PostsController < ApplicationController
 	before_action :authenticate_user!
-	before_action :set_post,only:[:show, :edit, :update, :destroy] 
+	before_action :set_post,only:[:show, :edit, :update, :destroy, :like] 
 	before_action :owned_post, only: [:edit, :update, :destroy]
 	
 	def index
-		@posts = Post.all
+		@posts = Post.all.order('created_at DESC')	
 	end
 
 	def new
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
 
     	if @post.save
       		
-      		redirect_to posts_path
+      		redirect_to root_path
     	else
       		
       		render :new
@@ -48,7 +48,14 @@ class PostsController < ApplicationController
     	redirect_to root_path
   	end
 
-  
+  	def like
+  		if @post.liked_by current_user
+  			respond_to do |format|
+  				format.html{ redirect_to :back }
+  				format.js
+  			end
+  		end
+  	end
 
 	private
 	def post_params
