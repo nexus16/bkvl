f46cda178e4046c0da9419adc3bb48aa4747586c
TranDHa <ennino1234@gmail.com>
2016-04-05 13:36:49 +0700

    update profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+
   devise_for :users, :controllers => { registrations: 'registrations' }  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -57,7 +59,9 @@ Rails.application.routes.draw do
 
 
   root 'posts#index'
-  
+  get ':user_name', to: 'profiles#show', as: :profile
+  get ':user_name/edit', to: 'profiles#edit', as: :edit_profile
+  patch ':user_name/edit', to: 'profiles#update', as: :update_profile
   resources :posts do  
     resources :comments
   end 
