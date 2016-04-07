5edb43ad1eddac17f529cbebdb448839469f509b
TranDHa <ennino1234@gmail.com>
2016-04-07 21:40:32 +0700

    change password

--- config/routes.rb
+++ config/routes.rb
@@ -67,6 +67,10 @@ Rails.application.routes.draw do
     member do 
       get 'like'
     end
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'    
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'            
+  end
   end 
   
 end
